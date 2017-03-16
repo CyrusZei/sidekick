f9095aaaae6cc851bd226c53babe42748812fb4d
Cyrus Zei <cyruszei@gmail.com>
2017-03-16 15:09:09 +0100

    structure base

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ import './App.scss';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h3>it works</h3>
+      <div className='container'>
+        <div className='row'>
+          <div className='block _10'>
+            <div className='row'>
+              <div className='block _100'>
+                <p>meny</p>
+              </div>
+              <div className='block _100'>
+                <p>spark</p>
+              </div>
+            </div>
+          </div>
+          <div className='block _90'>
+            <h3>it works</h3>
+          </div>
+        </div>
       </div>
+
     );
   }
 }
